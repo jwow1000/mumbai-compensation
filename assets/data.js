2c87c1c534b1095fb739db49d5de1441ccec4989
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-02-07 09:19:38 -0500

    more JSON work

--- assets/data.js
+++ assets/data.js
@@ -3,20 +3,15 @@ export const dataMap = {
     "label": "Does your livelihood depend on fishing?",
     "choices": [
       {"label": "Yes", "link": "ids"},
-      {"label": "No", "link": "end"},
+      {"label": "No", "link": "globalTerminus"},
     ]
   },
 
-  "end": {
-    "label": "You do not get compensation, try again",
-    "choices": [
-      {"label": "Try Again", "link": "start"}
-    ] 
-  },
   // you need all fuve
 
   "ids": {
-    "label": "Can you present all of these documents",
+    "label": "Can you present all of these documents?",
+    "checklist": "true",
     "choices": [
       {"label": "Compensation Claim form", "link": "boat"},
       {"label": "Ration Card", "link": "boat"},
@@ -36,7 +31,8 @@ export const dataMap = {
   },
   // need all five here too
   "boatIds": {
-    "label": "Choose a way to identify your boat",
+    "label": "Can you present all of these documents for your boat?",
+    "checklist": "true",
     "choices": [
       {"label": "Copy of boat license", "link": "boatSize"},
       {"label": "Boat registration documents", "link": "boatSize"},
@@ -50,23 +46,106 @@ export const dataMap = {
   "boatSize": {
     "label": "What size is your boat?",
     "choices": [
-      {"label": "Hodi", "link": "hodi"},
-      {"label": "1Cyl", "link": "1cyl"},
-      {"label": "2Cyl", "link": "2cyl"},
-      {"label": "3Cyl", "link": "3cyl"},
-      {"label": "4Cyl", "link": "4cyl"},
-      {"label": "6Cyl", "link": "6cyl"},
+      {"label": "Hodi", "link": boatTerminus.hodi},
+      {"label": "1Cyl", "link": boatTerminus.cyl1},
+      {"label": "2Cyl", "link": boatTerminus.cyl2},
+      {"label": "3Cyl", "link": boatTerminus.cyl3},
+      {"label": "4Cyl", "link": boatTerminus.cyl4},
+      {"label": "6Cyl", "link": boatTerminus.cyl6},
+    ]
+  },
+  
+  "boatTerminus": {
+    "hodi": {
+      "label": `₹ 1,75,000.00 per year for the 5 year duration of construction.
+
+      Permanent loss of livelihood and income from fishing.
+
+      A host of welfare benefits aimed at inproving your livelihood as part of the fishing community...`,
+
+      "terminus": "true"
+    },
+    "cyl1": {
+      "label": `₹ 2,29,810.39 per year for the 5 year duration of construction.
+
+
+      Permanent loss of livelihood and income from fishing.
+
+      A host of welfare benefits aimed at inproving your livelihood as part of the fishing community..`,
+
+      "terminus": "true"
+    },
+    "cyl2": {
+      "label": `₹ 2,99,795.11 per year for the 5 year duration of construction.
+
+
+      Permanent loss of livelihood and income from fishing.
+
+      A host of welfare benefits aimed at inproving your livelihood as part of the fishing community..`,
+
+      "terminus": "true"
+    },
+    "cyl3": {
+      "label": `₹ 3,29,774.62 per year for the 5 year duration of construction.
+
+      Permanent loss of livelihood and income from fishing.
+
+      A host of welfare benefits aimed at inproving your livelihood as part of the fishing community..`,
+
+      "terminus": "true"
+    },
+    "cyl4": {
+      "label": `₹ 4,15,366.99 per year for the 5 year duration of construction.
+
+
+      Permanent loss of livelihood and income from fishing.
+
+      A host of welfare benefits aimed at inproving your livelihood as part of the fishing community..`,
+
+      "terminus": "true"
+    },
+    "cyl6": {
+      "label": `₹ 4,56,903.68 per year for the 5 year duration of construction.
+      
+      Permanent loss of livelihood and income from fishing.
+
+      A host of welfare benefits aimed at inproving your livelihood as part of the fishing community..`,
+
+      "terminus": "true"
+    },
+  },
+
+  "nonBoats": {
+    "label": "Which of these practices best describes your connection to fishing?",
+    "choices": [
+      {"label": "Tandels", "link": "fisherSociety1"},
+      {"label": "Khalashi", "link": "fisherSociety1"},
+      {"label": "Handpickers", "link": "boat"},
+      {"label": "Castnets", "link": "boat"},
+      {"label": "Fish Seller", "link": "boat"},
+    ]  
+  },
+  "fisherSociety1": {
+    "label": "Are you in good standing with the fishers society?",
+    "choices": [
+      {"label": "Yes", "link": ""}
     ]
   },
 
-  // "hodi": {
-  //   "label": `₹ 1,75,000.00 per year for the 5 year duration of construction.
+  "fisherSociety1Yes": {
+    "label": `Permanent loss of livelihood and income from fishing.
 
-  //     Permanent loss of livelihood and income from fishing.
+    Compensation for 5 years of the project at approximately₹ 1,50,000.00 per year.
 
-  //     A host of community welfare benefits that no longer benefit you because you can no longer sell fish because there are no fish.`,
-  //   ""
-  // }
+    A host of welfare benefits aimed at inproving your livelihood as part of the fishing community..`,
+    "terminus": "true", 
+  },
 
+  "globalTerminus": {
+    "label": "You are not eligible for compensation",
+    "choices": [
+      {"label": "Replay", "link": "start"},
+    ]
+  }
 
 }
